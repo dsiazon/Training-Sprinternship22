6da05e7a5b8848bed5bfc9b2eac6ad387643e64f
dsiazon <105190107+dsiazon@users.noreply.github.com>
2022-05-09 13:24:55 -0500

    created_classCar

--- exercises/oop_helloworld.py
+++ exercises/oop_helloworld.py
@@ -1,4 +1,15 @@
 # create your class here
+class Car:
+    # instance attributes
+    def __init__(self, model, speed):
+        self.model = model
+        self.speed = speed
+    
+    # instance method
+    def getModel(self):
+        return self.model
+    def setSpeed(self, speed):
+        self.speed = speed
 
 if __name__ == '__main__':
     bmw = Car('BMW', 30.3)
