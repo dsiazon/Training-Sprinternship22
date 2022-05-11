77c562f7002d9669639211d3a9770f0ca4b7638a
dsiazon <105190107+dsiazon@users.noreply.github.com>
2022-05-11 11:04:12 -0500

    js excercises

--- exercises/JavaScript/program.js
+++ exercises/JavaScript/program.js
@@ -2,9 +2,17 @@ var body;
 
 // implement onload function
 
+window.onload = function(){
+    body = document.querySelector("body");
+}
 
 // implement changeColorBlue
 
-
+window.changeColorBlue = function(){
+    body.style.backgroundColor = "#a8ceff";
+}
 // implement changeColorPurple
 
+window.changeColorRed = function(){
+    body.style.backgroundColor = "#660808";
+}
